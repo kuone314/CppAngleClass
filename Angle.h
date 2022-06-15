454eb669576f3e84e4cb3a9adfc197e3dcf23080
kuone <kuone@example.com>
2022-06-16 00:08:05 +0900

    [fix] 関数に noexcept を追加

--- Angle.h
+++ Angle.h
@@ -15,13 +15,13 @@ public:
 public:
 	constexpr double Rad() const noexcept { return m_rad; }
 	constexpr double Deg() const noexcept { return m_rad * rate_r_to_d ; }
-	
+
 public:
-	static constexpr Angle Zero    () { return Rad( 0        ) ; }
-	static constexpr Angle Vertical() { return Rad( M_PI / 2 ) ; }
-	static constexpr Angle Flat    () { return Rad( M_PI     ) ; }
-	static constexpr Angle Full    () { return Rad( 2 * M_PI ) ; }
-	
+	static constexpr Angle Zero    () noexcept { return Rad( 0 ) ; }
+	static constexpr Angle Vertical() noexcept { return Rad( M_PI / 2 ) ; }
+	static constexpr Angle Flat    () noexcept { return Rad( M_PI ) ; }
+	static constexpr Angle Full    () noexcept { return Rad( 2 * M_PI ) ; }
+
 public:
 	// Only the following 5 parameter sets are valid.
 	// Normalize<   0, 360>() ;
@@ -30,25 +30,25 @@ public:
 	// Normalize<   0,  90>() ;
 	// Normalize< -90,  90>() ;
 	template<int stt_deg, int end_deg>
-	Angle Normalize() const = delete;
+	Angle Normalize() const noexcept = delete;
 
 public:
 	constexpr Angle operator +() const { return *this ; }
 	constexpr Angle operator -() const { return Angle{ -m_rad } ; }
 
 public:
-	constexpr bool operator == ( const Angle& a ) const { return m_rad == a.m_rad ; }
-	constexpr bool operator != ( const Angle& a ) const { return m_rad != a.m_rad ; }
-	constexpr bool operator <  ( const Angle& a ) const { return m_rad  < a.m_rad ; }
-	constexpr bool operator >  ( const Angle& a ) const { return m_rad  > a.m_rad ; }
-	constexpr bool operator <= ( const Angle& a ) const { return m_rad <= a.m_rad ; }
-	constexpr bool operator >= ( const Angle& a ) const { return m_rad >= a.m_rad ; }
-	
+	constexpr bool operator == ( const Angle& a ) const noexcept { return m_rad == a.m_rad ; }
+	constexpr bool operator != ( const Angle& a ) const noexcept { return m_rad != a.m_rad ; }
+	constexpr bool operator <  ( const Angle& a ) const noexcept { return m_rad  < a.m_rad ; }
+	constexpr bool operator >  ( const Angle& a ) const noexcept { return m_rad  > a.m_rad ; }
+	constexpr bool operator <= ( const Angle& a ) const noexcept { return m_rad <= a.m_rad ; }
+	constexpr bool operator >= ( const Angle& a ) const noexcept { return m_rad >= a.m_rad ; }
+
 public:
-	Angle& operator += ( const Angle& a ) { m_rad += a.Rad() ; return *this ; }
-	Angle& operator -= ( const Angle& a ) { m_rad -= a.Rad() ; return *this ; }
-	Angle& operator *= ( const double val ) { m_rad *= val ; return *this ; }
-	Angle& operator /= ( const double val ) { m_rad /= val ; return *this ; }
+	Angle& operator += ( const Angle& a ) noexcept { m_rad += a.Rad() ; return *this ; }
+	Angle& operator -= ( const Angle& a ) noexcept { m_rad -= a.Rad() ; return *this ; }
+	Angle& operator *= ( const double val ) noexcept { m_rad *= val ; return *this ; }
+	Angle& operator /= ( const double val ) noexcept { m_rad /= val ; return *this ; }
 
 private:
 	explicit constexpr Angle( const double rad ) noexcept :m_rad{ rad } {}
@@ -62,20 +62,20 @@ private:
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-constexpr inline Angle operator + ( const Angle& a1, const Angle& a2 ) { return Angle::Rad( a1.Rad() + a2.Rad() ) ; }
-constexpr inline Angle operator - ( const Angle& a1, const Angle& a2 ) { return Angle::Rad( a1.Rad() - a2.Rad() ) ; }
-constexpr inline Angle operator * ( const double val, const Angle& a ) { return Angle::Rad( val * a.Rad() ) ; }
-constexpr inline Angle operator * ( const Angle& a, const double val ) { return Angle::Rad( a.Rad() * val ) ; }
-constexpr inline Angle operator / ( const Angle& a, const double val ) { return Angle::Rad( a.Rad() / val ) ; }
+constexpr inline Angle operator + ( const Angle& a1, const Angle& a2 ) noexcept { return Angle::Rad( a1.Rad() + a2.Rad() ) ; }
+constexpr inline Angle operator - ( const Angle& a1, const Angle& a2 ) noexcept { return Angle::Rad( a1.Rad() - a2.Rad() ) ; }
+constexpr inline Angle operator * ( const double val, const Angle& a ) noexcept { return Angle::Rad( val * a.Rad() ) ; }
+constexpr inline Angle operator * ( const Angle& a, const double val ) noexcept { return Angle::Rad( a.Rad() * val ) ; }
+constexpr inline Angle operator / ( const Angle& a, const double val ) noexcept { return Angle::Rad( a.Rad() / val ) ; }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-inline double sin( const Angle& angle ) { return std::sin( angle.Rad() ) ; }
-inline double cos( const Angle& angle ) { return std::cos( angle.Rad() ) ; }
-inline double tan( const Angle& angle ) { return std::tan( angle.Rad() ) ; }
+inline double sin( const Angle& angle ) noexcept { return std::sin( angle.Rad() ) ; }
+inline double cos( const Angle& angle ) noexcept { return std::cos( angle.Rad() ) ; }
+inline double tan( const Angle& angle ) noexcept { return std::tan( angle.Rad() ) ; }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 template<>
-Angle Angle::Normalize<0, 360>() const
+Angle Angle::Normalize<0, 360>() const noexcept
 {
 	constexpr auto full_rad = Full().Rad() ;
 	const auto mod = std::fmod( m_rad, full_rad ) ;
@@ -84,7 +84,7 @@ Angle Angle::Normalize<0, 360>() const
 }
 
 template<>
-Angle Angle::Normalize<-180, 180>() const
+Angle Angle::Normalize<-180, 180>() const noexcept
 {
 	auto result = Normalize<0, 360>() ;
 	if ( result >= Flat() ) { result -= Full() ; }
@@ -92,7 +92,7 @@ Angle Angle::Normalize<-180, 180>() const
 }
 
 template<>
-Angle Angle::Normalize<0, 180>() const
+Angle Angle::Normalize<0, 180>() const noexcept
 {
 	auto result = Normalize<-180, 180>() ;
 	if ( result < Zero() ) { result *= -1 ; }
@@ -100,7 +100,7 @@ Angle Angle::Normalize<0, 180>() const
 }
 
 template<>
-Angle Angle::Normalize<-90, 90>() const
+Angle Angle::Normalize<-90, 90>() const noexcept
 {
 	auto result = Normalize<-180, 180>() ;
 	if ( result >= Vertical() ) {
@@ -112,7 +112,7 @@ Angle Angle::Normalize<-90, 90>() const
 }
 
 template<>
-Angle Angle::Normalize<0, 90>() const
+Angle Angle::Normalize<0, 90>() const noexcept
 {
 	auto result = Normalize<-90, 90>() ;
 	if ( result < Zero() ) { result *= -1 ; }
