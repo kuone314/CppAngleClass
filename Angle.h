77bfd06c97a14929927e70a1e4c571f8fd38a24c
kuone <kuone@example.com>
2022-06-15 23:39:18 +0900

    [feat] 定義,値の取得の実装

--- Angle.h
+++ Angle.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <cmath>
+
 namespace math {
+
+class Angle {
+public:
+	constexpr Angle() noexcept :Angle{ 0 } {}
+	static constexpr Angle Rad( const double rad ) noexcept { return Angle{ rad } ; }
+	static constexpr Angle Deg( const double deg ) noexcept { return Angle{ deg * rate_d_to_r } ; }
+
+public:
+	constexpr double Rad() const noexcept { return m_rad; }
+	constexpr double Deg() const noexcept { return m_rad * rate_r_to_d ; }
+
+private:
+	explicit constexpr Angle( const double rad ) noexcept :m_rad{ rad } {}
+
+private:
+	static constexpr double rate_d_to_r = M_PI / 180;
+	static constexpr double rate_r_to_d = 180 / M_PI ;
+
+private:
+	double m_rad{};
+};
+
 
 } // namespace math 
--- Develop/Sample.cpp
+++ Develop/Sample.cpp
@@ -1,11 +1,24 @@
 
 #include "../Angle.h"
+#include <assert.h>
 
 
+bool eq( const double v_1, const double v_2 ) { return abs( v_1 - v_2 ) < 0.001; }
 
 int main()
 {
-	{
-	}
+	// Define, Get Value.
+	const auto angle_0 = math::Angle{};
+	assert( eq( angle_0.Deg(), 0 ) );
+	assert( eq( angle_0.Rad(), 0 ) );
+
+	const auto angle_30 = math::Angle::Deg( 30 );
+	assert( eq( angle_30.Deg(), 30 ) );
+	assert( eq( angle_30.Rad(), M_PI / 6 ) );
+
+	const auto angle_45 = math::Angle::Rad( M_PI / 4 );
+	assert( eq( angle_45.Deg(), 45 ) );
+	assert( eq( angle_45.Rad(), M_PI / 4 ) );
+
 
 }
